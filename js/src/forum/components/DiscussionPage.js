09765c3452a3142c816a41cf2c12337a3a41373d
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-10-07 16:59:06 -0400

    Directly get target from state
    There's no reason to abstract away passing in the target post, as everything else comes directly from the state.

--- js/src/forum/components/DiscussionPage.js
+++ js/src/forum/components/DiscussionPage.js
@@ -84,7 +84,6 @@ export default class DiscussionPage extends Page {
                     {PostStream.component({
                       discussion,
                       stream: this.stream,
-                      targetPost: this.stream.targetPost,
                       onPositionChange: this.positionChanged.bind(this),
                     })}
                   </div>
